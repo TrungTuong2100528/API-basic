591afe95513c4a2e66c416dbd687d829ccb38e73
TrungTuong2100528 <cttuong2100528@gmail.com>
2026-01-04 21:20:12 +0700

    v3.1 Finish feature SearchInventory

--- FrontEnd/hoc-gadget-shop/src/app/AppComponents/inventory/inventory.component.ts
+++ FrontEnd/hoc-gadget-shop/src/app/AppComponents/inventory/inventory.component.ts
@@ -13,69 +13,70 @@ import { DialogBoxComponent } from '../../AppComponent/dialog-box/dialog-box.com
 export class InventoryComponent {
   //Lấy service HttpClient để Gọi API ; tương đương với constructor(private httpClient: HttpClient) {}
   httpClient = inject(HttpClient)
-  productIDToDelete: number =0;
+  productIDToDelete: number = 0;
   private modalService = inject(NgbModal)
 
   //Get
   inventoryDetails: any; //Lưu dữ liệu API trả về
 
+
   inventoryData = {
-    productID:"",
-    productName:"",
-    availableQty:0,
-    reOderPoint:0
+    productID: "",
+    productName: "",
+    availableQty: 0,
+    reOderPoint: 0
   }
 
-  ngOnInit(){ //Hàm chạy khi component được load
-     this.getInventoryDetails();
+  ngOnInit() { //Hàm chạy khi component được load
+    this.getInventoryDetails();
   }
 
 
-  getInventoryDetails(){
-    let aipUrl="https://localhost:7243/api/Inventory";
+  getInventoryDetails() {
+    let aipUrl = "https://localhost:7243/api/Inventory";
 
-     this.httpClient.get(aipUrl).subscribe(data=>{ //Angular gửi HTTP GET; subscribe() để: Nhận dữ liệu async
+    this.httpClient.get(aipUrl).subscribe(data => { //Angular gửi HTTP GET; subscribe() để: Nhận dữ liệu async
       this.inventoryDetails = data; //Gán dữ liệu cho biến
       console.log(this.inventoryDetails) //Log để debug
-     })
+    })
 
-     // Thiết lập lại sao khi put
-     this.inventoryData ={
-      productID:"",
-      productName:"",
-      availableQty:0,
-      reOderPoint:0
-     }
-     this.disavledProductIdInput = false;
-  }
-
-
-  disavledProductIdInput= false;
-
-  oSubmit(): void{ //Chạy khi bấm nút Submit
-    
-    let aipUrl="https://localhost:7243/api/Inventory";
-    let httpOptions={ //Cấu hình HTTP Header
-        headers: new HttpHeaders({
-          Authorization: "my-auth-token", //Token (demo)
-          "Content-Type": "application/json" //Báo cho API biết gửi JSON
-        })
+    // Thiết lập lại sao khi put
+    this.inventoryData = {
+      productID: "",
+      productName: "",
+      availableQty: 0,
+      reOderPoint: 0
     }
-  //Điều kiện trước khi post or put
-  if(this.disavledProductIdInput == true){
-    this.httpClient.put(aipUrl, this.inventoryData,httpOptions).subscribe({
-      next: v=> console.log(v), //API trả dữ liệu thành công
-      error: e=> console.log(e), //API lỗi (400 / 500)
-      complete: () => { //Request hoàn tất
-        alert("Form Submitted successfully" + JSON.stringify(this.inventoryData));
-        this.getInventoryDetails();
-      }
-    });
+    this.disavledProductIdInput = false;
   }
-  else{
-      this.httpClient.post(aipUrl, this.inventoryData,httpOptions).subscribe({
-        next: v=> console.log(v), 
-        error: e=> console.log(e), 
+
+
+  disavledProductIdInput = false;
+
+  oSubmit(): void { //Chạy khi bấm nút Submit
+
+    let aipUrl = "https://localhost:7243/api/Inventory";
+    let httpOptions = { //Cấu hình HTTP Header
+      headers: new HttpHeaders({
+        Authorization: "my-auth-token", //Token (demo)
+        "Content-Type": "application/json" //Báo cho API biết gửi JSON
+      })
+    }
+    //Điều kiện trước khi post or put
+    if (this.disavledProductIdInput == true) {
+      this.httpClient.put(aipUrl, this.inventoryData, httpOptions).subscribe({
+        next: v => console.log(v), //API trả dữ liệu thành công
+        error: e => console.log(e), //API lỗi (400 / 500)
+        complete: () => { //Request hoàn tất
+          alert("Form Submitted successfully" + JSON.stringify(this.inventoryData));
+          this.getInventoryDetails();
+        }
+      });
+    }
+    else {
+      this.httpClient.post(aipUrl, this.inventoryData, httpOptions).subscribe({
+        next: v => console.log(v),
+        error: e => console.log(e),
         complete: () => {
           alert("Form Submitted successfully" + JSON.stringify(this.inventoryData));
           this.getInventoryDetails();
@@ -85,25 +86,25 @@ export class InventoryComponent {
   }
 
   // Delete
-  openConfirmDialog(productID: number){
+  openConfirmDialog(productID: number) {
     this.productIDToDelete = productID;
     console.log(this.productIDToDelete);
-      this.modalService.open(DialogBoxComponent).result.then(data=>{
-        if(data.event == "confirm"){
-          this.deleteInventory();
-        }
-      })
+    this.modalService.open(DialogBoxComponent).result.then(data => {
+      if (data.event == "confirm") {
+        this.deleteInventory();
+      }
+    })
   }
-  deleteInventory():void{
-    let aipUrl="https://localhost:7243/api/Inventory?productId="+this.productIDToDelete;
+  deleteInventory(): void {
+    let aipUrl = "https://localhost:7243/api/Inventory?productId=" + this.productIDToDelete;
 
-    this.httpClient.delete(aipUrl).subscribe(data=>{
+    this.httpClient.delete(aipUrl).subscribe(data => {
       this.getInventoryDetails();
     });
   }
 
   //Put
-  popularFormForEdit(inventory : any){
+  popularFormForEdit(inventory: any) {
     this.inventoryData.productID = inventory.productID;
     this.inventoryData.productName = inventory.productName;
     this.inventoryData.availableQty = inventory.availableQty;
@@ -111,5 +112,25 @@ export class InventoryComponent {
 
     //Ngăn chặn nhập
     this.disavledProductIdInput = true;
+  }
+
+  //Lấy giá trị nhập tìm kiếm
+  enterSearchData: string = "";
+  // Lưu giá trị productname 
+  SearchProductName: string = "";
+  //search
+
+  SearchInventory(productName: string) {
+    this.SearchProductName = productName;
+    if (this.SearchProductName == "") {
+      this.getInventoryDetails();
+      return;
+    }else{
+      let aipUrl = "https://localhost:7243/api/Inventory/search?productName=" + this.SearchProductName;
+      this.httpClient.get(aipUrl).subscribe(data => {
+        this.inventoryDetails = data;
+        console.log(this.inventoryDetails)
+      })
+    }
   }
 }
